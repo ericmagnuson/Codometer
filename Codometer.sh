0a112a451ef88c32ee732a398a3a0a4485e38f9c
Eric Magnuson <eric@ericmagnuson.me>
2013-09-30 02:15:09 -0600

    User can specify chars per inch. Colorzed output.

--- Codometer.sh
+++ Codometer.sh
@@ -6,7 +6,7 @@
 #
 # Calculate how many miles of code are written in a directory and its
 # subdirectories, as if each character were one after the other on one
-# line. Assumes 12 characters per 1 inch of screen space.
+# line. Defaults to 12 characters per 1 inch of screen space.
 #
 # Instructions: Save and run this script in the topmost directory of
 # your project. Alternatively, run the script from Github (without
@@ -14,12 +14,33 @@
 #
 # bash < <(curl -s https://raw.github.com/ericmagnuson/Codometer/master/Codometer.sh)
 
-chars=$(find . \( ! -regex '.*/\..*' \) -type f -exec wc -m {} \; 2> /dev/null | awk '{total += $1} END{print total}')
+blue=$(tput setaf 6)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+nc=$(tput sgr0) # No Color
 
-inches=$(echo "scale=2; $chars / 12" | bc)
-feet=$(echo "scale=2; $chars / 12 / 12" | bc)
-miles=$(echo "scale=2; $chars / 12 / 12 / 5280" | bc)
+read -e -p "${blue}In your text editor, how many characters
+fit into one inch on your screen (if no
+answer is given, this will default to 12)?${nc} " chars_per_inch
+chars_per_inch=${chars_per_inch:-12}
 
-echo "$miles miles of code"
-echo "...or $feet feet of code"
-echo "...or $inches inches of code."
+if [[ $chars_per_inch =~ ^-?[0-9]+$ ]]; then
+
+	chars=$(find . \( ! -regex '.*/\..*' \) \
+		! -name 'Codometer.sh' -type f -exec wc -m {} \; 2> /dev/null \
+		| awk '{total += $1} END{print total}')
+
+	inches=$(echo "scale=2; $chars / $chars_per_inch" | bc)
+	feet=$(echo "scale=2; $chars / $chars_per_inch / 12" | bc)
+	miles=$(echo "scale=2; $chars / $chars_per_inch / 12 / 5280" | bc)
+
+	echo "${green}$miles miles of code"
+	echo "...or $feet feet of code"
+	echo "...or $inches inches of code.${nc}"
+
+else
+
+	echo "${red}Please try again with a valid number.${nc}"
+	exit;
+
+fi
