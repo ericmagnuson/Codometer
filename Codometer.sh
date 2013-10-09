c630a8f53cd9549aec633a73352de9d901785c4c
Eric Magnuson <eric@ericmagnuson.me>
2013-10-09 00:25:11 -0600

    Added progress indicator

--- Codometer.sh
+++ Codometer.sh
@@ -20,17 +20,38 @@ green=$(tput setaf 2)
 red=$(tput setaf 1)
 nc=$(tput sgr0) # No Color
 
+function progress() {
+	echo ""
+	echo -n "Calculating the distance..."
+	while true
+	do
+		echo -n "."
+		sleep 1
+	done
+}
+
+# Ask for chars per inch variable
 read -e -p "${blue}In your text editor, how many characters
 fit into one inch on your screen (if no
 answer is given, this will default to 12)?${nc} " chars_per_inch
 chars_per_inch=${chars_per_inch:-12}
 
+# If $chars_per_inch is an int
 if [[ $chars_per_inch =~ ^-?[0-9]+$ ]]; then
+
+	# Show progress text and save PID to kill it later
+	progress &
+	PROGRESS_PID=$!
 
 	# Get the number of chars in the entire directory
 	chars=$(find . \( ! -regex '.*/\..*' \) \
 		! -name 'Codometer.sh' -type f -exec wc -m {} \; 2> /dev/null \
 		| awk '{total += $1} END{print total}')
+
+	# Stop the progress text
+	echo ""
+	kill $PROGRESS_PID
+	wait $! 2>/dev/null
 
 	# Check if bc is installed for doing decimal work.
 	# If not, we will fall back to expr (no decimals).
